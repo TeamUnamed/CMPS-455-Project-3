cb4b5725ab5179f9a629296a9e43b22eca09c131
malfact <themalfact@gmail.com>
2023-04-10 16:51:43 -0500

    Basic Task info for testing
    Signed-off-by: malfact <themalfact@gmail.com>

--- src/main/java/net/unamed/cmps455/project3/TaskThread.java
+++ src/main/java/net/unamed/cmps455/project3/TaskThread.java
@@ -2,4 +2,9 @@ package net.unamed.cmps455.project3;
 
 public class TaskThread extends Thread {
 
+    public final String name;
+
+    public TaskThread (String name) {
+        this.name = name;
+    }
 }
