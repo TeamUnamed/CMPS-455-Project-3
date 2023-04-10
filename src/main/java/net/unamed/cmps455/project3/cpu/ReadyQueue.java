a48985fe5d422b386013944e005b8f9bd70c2d04
malfact <themalfact@gmail.com>
2023-04-10 16:52:12 -0500

    Basic ReadyQueue implementation. Not all functions are thread-safe
    Signed-off-by: malfact <themalfact@gmail.com>

--- src/main/java/net/unamed/cmps455/project3/cpu/ReadyQueue.java
+++ src/main/java/net/unamed/cmps455/project3/cpu/ReadyQueue.java
@@ -1,5 +1,56 @@
 package net.unamed.cmps455.project3.cpu;
 
+import net.unamed.cmps455.project3.TaskThread;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.concurrent.Semaphore;
+
 public class ReadyQueue {
 
+    private LinkedList<TaskThread> tasks;
+
+    Semaphore queueLock;
+
+    public ReadyQueue () {
+        queueLock = new Semaphore(1);
+        tasks = new LinkedList<>();
+    }
+
+    public void add (TaskThread taskThread) {
+        tasks.add(taskThread);
+    }
+
+    public boolean hasQueued() {
+        return tasks.size() > 0;
+    }
+
+    public Optional<TaskThread> processQueue(QueueProcessor processor) {
+        Optional<TaskThread> optional = Optional.empty();
+        try {
+            queueLock.acquire();
+
+            optional = Optional.ofNullable(processor.process(tasks.iterator()));
+
+            queueLock.release();
+        } catch (InterruptedException e) {
+            System.out.println("Processing interrupted");
+        }
+
+        return optional;
+    }
+
+    /**
+     * Functional Interface for processing the contents of a {@link ReadyQueue}.
+     * @see QueueProcessor#process(Iterator) 
+     */
+    public interface QueueProcessor {
+        /**
+         * Process the contents of a ReadyQueue.
+         * @param iterator An Iterator belonging to a ReadyQueue.
+         * @return A {@link TaskThread} from processing or {@code null}.
+         */
+        TaskThread process(Iterator<TaskThread> iterator);
+    }
 }
