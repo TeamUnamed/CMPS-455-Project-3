ec9531d87c0b475db16a4e81515b9fa061055ff9
malfact <themalfact@gmail.com>
2023-04-10 16:52:34 -0500

    Basic implementation for testing
    Signed-off-by: malfact <themalfact@gmail.com>

--- src/main/java/net/unamed/cmps455/project3/cpu/Dispatcher.java
+++ src/main/java/net/unamed/cmps455/project3/cpu/Dispatcher.java
@@ -1,11 +1,15 @@
 package net.unamed.cmps455.project3.cpu;
 
 import net.unamed.cmps455.project3.OperatingSystem;
+import net.unamed.cmps455.project3.TaskThread;
+
+import java.util.Iterator;
+import java.util.Optional;
 
 /**
  * Abstract representation of a Dispatcher
  */
-public abstract class Dispatcher extends Thread {
+public class Dispatcher extends Thread {
 
     private final OperatingSystem os;
     private final Core cpu;
@@ -17,6 +21,34 @@ public abstract class Dispatcher extends Thread {
 
     @Override
     public void run() {
+        System.out.println("Dispatcher Started!");
+        ReadyQueue readyQueue = os.getReadyQueue();
+        while(readyQueue.hasQueued()) {
+            Optional<TaskThread> optional = readyQueue.processQueue(this::getNextTaskThread);
+            optional.ifPresentOrElse(
+                    (taskThread) -> System.out.println("Task Thread Found: " + taskThread.name),
+                    () -> System.out.println("No Task Threads")
+            );
+        }
 
+        System.out.println("Ready Queue Empty :: Bye!");
+    }
+
+    /*
+     * Uses FCFS test code
+     */
+    /**
+     * @see ReadyQueue.QueueProcessor#process(Iterator) 
+     */
+    private TaskThread getNextTaskThread(Iterator<TaskThread> iterator) {
+        TaskThread taskThread = null;
+
+        // Get the first TaskThread from queue and remove
+        if (iterator.hasNext()) {
+            taskThread = iterator.next();
+            iterator.remove();
+        }
+
+        return taskThread;
     }
 }
