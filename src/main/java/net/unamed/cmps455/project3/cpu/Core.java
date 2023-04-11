997d54d60c81011ba20e664d3969e0cbbe402bd1
malfact <themalfact@gmail.com>
2023-04-11 00:20:27 -0500

    Implemented logic for CPU Core
    Signed-off-by: malfact <themalfact@gmail.com>

--- src/main/java/net/unamed/cmps455/project3/cpu/Core.java
+++ src/main/java/net/unamed/cmps455/project3/cpu/Core.java
@@ -1,10 +1,50 @@
 package net.unamed.cmps455.project3.cpu;
 
-public class Core extends Thread {
+import net.unamed.cmps455.project3.OperatingSystem;
+import net.unamed.cmps455.project3.Process;
 
-    @Override
-    public void run() {
+import java.util.Optional;
 
+public class Core {
+
+    private final int ssid;
+    private final OperatingSystem os;
+
+    private Process process;
+    private Thread processThread;
+
+    private Runnable callback;
+
+    public Core (int ssid, OperatingSystem os) {
+        this.ssid = ssid;
+        this.os = os;
     }
 
+    public int getSSID() {
+        return ssid;
+    }
+
+    public Optional<Process> scheduleProcess(Process process) {
+        Process oldProcess = this.process;
+        if (this.process != null) {
+            synchronized (callback) {
+                callback.notifyAll();
+            }
+        }
+
+        this.process = process;
+        this.process.setCallback(this::onTaskFinish);
+        processThread = new Thread(process);
+        processThread.start();
+
+        return Optional.ofNullable(oldProcess);
+    }
+
+    public void registerCallback(Runnable runnable) {
+        this.callback = runnable;
+    }
+
+    private void onTaskFinish(Process task) {
+        callback.run();
+    }
 }
